ebf42623d0f400fc41192f3597ce79cad542183f
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2023-02-01 00:54:44 +0000

    am pm conversion

--- src/main/java/HackerRank.java
+++ src/main/java/HackerRank.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,12 +28,28 @@ public class HackerRank {
         Collections.sort(arr);
         long min = 0;
         long max = 0;
-        for (int i = 0; i < arr.size(); i ++){
+        for (int i = 0; i < arr.size(); i++) {
             min += arr.get(i);
             max += arr.get(i);
         }
         min -= arr.get(arr.size() - 1);
         max -= arr.get(0);
         System.out.println(min + " " + max);
+    }
+
+    public static String timeConversion(String s) {
+        char[] charArray = s.toCharArray();
+        int pOrA = charArray.length - 2;
+        if (charArray[pOrA] == 'P' && charArray[1] == '2' && charArray[0] == '1') {
+        } else if (charArray[pOrA] == 'P') {
+            charArray[0] += 1;
+            charArray[1] += 2;
+        } else if (charArray[pOrA] == 'A' && charArray[1] == '2') {
+            charArray[0] -= 1;
+            charArray[1] -= 2;
+        }
+        char[] finalCharArray = Arrays.copyOfRange(charArray, 0, pOrA);
+        String string = new String(finalCharArray);
+        return string;
     }
 }
