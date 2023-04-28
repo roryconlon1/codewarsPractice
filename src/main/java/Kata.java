e7b0f658e25e8a7f039d76f7c10dc86a0c89d9f6
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2023-04-28 14:13:49 +0100

    leetcode binary left + right

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -604,7 +604,25 @@ public class Kata {
         return n;
     }
 
+//    new version for time complexity, half range each time to narrow down to the first bad number
+//    if left is 1 and right is n, the limit, checks (1 + (10-1)) /2) so if 5 is bad then it becomes right
+//    and again halfs between left + 1 and then splits the new right until eventually iterates to find right one
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
 }
+
+
 
 
 
